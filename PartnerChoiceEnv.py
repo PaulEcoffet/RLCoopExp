b0e2ce07c21b2bd4a6295fc66d5cce53fb51f3c2
Paul Ecoffet <paul.ecoffet@isir.upmc.fr>
2020-12-04 15:07:59 +0100

    fix env and correct clip action for CMA

--- PartnerChoiceEnv.py
+++ PartnerChoiceEnv.py
@@ -28,6 +28,8 @@ def payoff(xi, xj, *, a=5, b=5, temp=None):
 class PartnerChoiceFakeSites(MultiAgentEnv):
 
     def __init__(self, env_config):
+        if "env_config" in env_config:
+            raise ValueError("Do not provide the full config but only the env_config")
         if "bad_site_prob" in env_config:
             raise ValueError("deprecated key bad_site_prob")
 
@@ -37,12 +39,13 @@ class PartnerChoiceFakeSites(MultiAgentEnv):
         self.new_x_each_interaction = env_config.get("new_x_each_interaction", True)
         self.max_action = env_config.get("max_action", 15.0)
         self.good_site_prob = env_config.get("good_site_prob", 1)
+        self.eval_mode = env_config.get("eval_mode", False)
         self.iteration_count = 0
         self.true_site = [True for i in range(self.nb_agents)]
         self.agents_names = ['inv' + '{:02d}'.format(i) for i in range(self.nb_agents)]
         self.agents_names += ['choice' + '{:02d}'.format(i) for i in range(self.nb_agents)]
         self.inv = [0.0 for i in range(self.nb_agents)]
-        self.cur_opp = [-1 for i in range(self.nb_agents)]
+        self.cur_opp = [None for i in range(self.nb_agents)]
 
         self.cur_its = np.array([0 for i in range(self.nb_agents)], dtype=int)
 
@@ -51,16 +54,24 @@ class PartnerChoiceFakeSites(MultiAgentEnv):
 
         self.site_action = np.linspace(0, self.max_action, self.nb_sites)
         self.site_acceptance_threshold = np.copy(self.site_action)
+        self.force_opp = None
 
-    def reset(self):
+        assert(not self.eval_mode or self.good_site_prob == 1)
+
+    def reset(self, *, force_cur_opp=None):
+        if force_cur_opp is not None and not self.eval_mode:
+            raise ValueError("force config only allowed in eval mode")
+        self.force_opp = force_cur_opp
         self.cur_its = np.array([0 for i in range(self.nb_agents)], dtype=int)
-        self.cur_opp = [None for i in range(self.nb_agents)]
+        self.cur_opp = [force_cur_opp for i in range(self.nb_agents)]
         # Make all individuals make their investment choice
         return {
             f'inv{i:02}': np.array([0], dtype=np.float32) for i in range(self.nb_agents)
         }
 
     def _find_opp(self):
+        if self.force_opp is not None:
+            return self.force_opp, True
         true_site = True
         if np.random.rand() < 1 - self.good_site_prob:
             true_site = False
@@ -80,7 +91,7 @@ class PartnerChoiceFakeSites(MultiAgentEnv):
             choice = f"choice{ind:02d}"
             # If we get a investment action
             if agent_name.startswith('inv'):
-                self.inv[ind] = action_dict[agent_name][0]
+                self.inv[ind] = np.asarray(action_dict[agent_name]).flatten()[0]
                 self.cur_opp[ind], self.true_site[ind] = self._find_opp()
                 obs[choice] = np.array([self.site_action[self.cur_opp[ind]], self.inv[ind]])
                 reward[choice] = 0  # dummy reward at init
@@ -88,7 +99,8 @@ class PartnerChoiceFakeSites(MultiAgentEnv):
                 self.cur_its[ind] += 1
 
                 curopp = self.cur_opp[ind]
-                info[inv] = {'inv': self.inv[ind], 'other': self.site_acceptance_threshold[curopp],
+                assert(isinstance(curopp, int))
+                info[inv] = {'inv': self.inv[ind], 'other': self.site_action[curopp],
                                 'accept': action_dict[choice]}
                 # if they both agree
                 if action_dict[choice] == 1 and self.inv[ind] >= self.site_acceptance_threshold[curopp]\
@@ -101,10 +113,11 @@ class PartnerChoiceFakeSites(MultiAgentEnv):
                     done[choice] = True
                     done[inv] = True
                     obs[inv] = np.array([0], dtype=np.float32)
-                    self.cur_its[ind] = self.max_it
+                    if not self.eval_mode:
+                        self.cur_its[ind] = self.max_it  # force end of experiment
                 else:  # if at least one disagree or not a real site
                     done[choice] = False
-                    self.cur_opp[ind] = self._find_opp()
+                    self.cur_opp[ind], self.true_site[ind] = self._find_opp()
                     if self.new_x_each_interaction:
                         obs[inv] = np.array([0], dtype=np.float32)
                         reward[inv] = 0
